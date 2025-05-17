739a138035d508bfefbf86668c3b874d9378e999
SantiagoEchavarria <santiagogil7112@gmail.com>
2025-05-16 20:39:10 -0500

    Aclaracion funcionamiento del test

--- simulador_energias/tests/locustfile.py
+++ simulador_energias/tests/locustfile.py
@@ -15,5 +15,7 @@ class CargaSimulador(HttpUser):
         # Simula un consumo_id existente en la base de datos
         consumo_id = random.randint(1, 10)
 
-        # Envía solicitud al endpoint que evalúa el umbral
+        # Envía solicitud al endpoint que evalúa el umbral 
+        # Realiza el proceso de verificar_umbral(request, id): que esta en simulador_energias/views y a su vez
+        #Este usa las funciones de utils.py del mismo modulo
         self.client.get(f"/api/consumo/{consumo_id}/verificar?inicio={fecha_inicio}&fin={fecha_fin}")
